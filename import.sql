658310adac56f6d8d22cebb6abeefa6ea3889546
dm441750-ops <dm441750@gmail.com>
2025-12-15 09:51:55 -0600

    Add files via upload

---
+++ import.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCTS (name, description, price, stock, image_url) VALUES
+('Playera Negra GL', 'Diseño urbano con logo', 450.0, 20, '/img/playera1.png'),
+('Sudadera Blanca GL', 'Minimal premium', 900.0, 8, '/img/sudadera1.png');
